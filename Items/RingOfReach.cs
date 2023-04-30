4769f21dbf58a56c282218b63ba6bce787109359
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-04-30 18:14:34 -0400

    Create RingOfReach.cs
    ring

---
+++ Items/RingOfReach.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class RingOfReach : MonoBehaviour
+{
+
+    public string Ring = "RingOfReach";
+    public float modification = 1.5f;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            // Get the PlayerController component from the player
+            PlayerController player = other.GetComponent<PlayerController>();
+
+            // Change the values of the variables in the PlayerController script
+            player.attackRange *= modification;
+            
+            // Destroy the battle axe object
+            Destroy(gameObject);
+        }
+    }
+}
+
