a8cf9a7329694fe4f7d1f071dc9cf27d0a3c2403
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-04-30 23:42:16 -0400

    Update HealthObject.cs

--- HealthObject.cs
+++ HealthObject.cs
@@ -3,23 +3,31 @@ using UnityEngine;
 public class HealthObject : MonoBehaviour
 {
     public int healthAmount = 100;
-    public float delayTime = 5.0f;
+    public float pickupDelay = 2f; // delay in seconds before the object can be picked up
+    private bool canBePickedUp = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && canBePickedUp)
         {
+            // Get the PlayerController component from the player
             PlayerController player = other.GetComponent<PlayerController>();
-            player.Heal(healthAmount);
-            print("Healed! Health is now " + player.health);
 
-            // Delay the destruction of the health object
-            Invoke("DestroyObject", delayTime);
+            // Change the values of the variables in the PlayerController script
+            player.Heal(healthAmount);
+            print("Healed! Health is now " + player.currentHealth);
+            // Destroy the HealthObject
+            Destroy(gameObject);
         }
     }
 
-    void DestroyObject()
+    private void Start()
     {
-        Destroy(gameObject);
+        Invoke("EnablePickup", pickupDelay);
     }
-}s
+
+    private void EnablePickup()
+    {
+        canBePickedUp = true;
+    }
+}
