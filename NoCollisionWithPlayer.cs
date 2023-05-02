7ebca0d0229b3fd6aa3789281bce0b7eb874308a
Jack-Tadych <61060120+Jack-Tadych@users.noreply.github.com>
2023-05-01 23:46:00 -0400

    Create NoCollisionWithPlayer.cs

---
+++ NoCollisionWithPlayer.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class NoCollisionWithPlayer : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Physics.IgnoreCollision(collision.collider, GetComponent<Collider>());
+        }
+    }
+}
